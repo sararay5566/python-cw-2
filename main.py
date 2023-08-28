3a85ea6a3324abbf6fdeb136631fa74b1f4ce26b
unkownnnn67959 <125199468+unkownnnn67959@users.noreply.github.com>
2023-08-28 20:05:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+name = input("what is your name?")
+age = input("how old are you?")
+print(f'My name is{name} and i am {age}')
+
+first_number = int(input ("enter first number"))
+seconed_number = int(input ("enter seconed number"))
+operation = int (input ("enter operation"))
+if operation=='+':
+    print("first_number+secound_number")
+elif operation== "-":
+    print(first_number-secound_number)
+elif operation =="*":
+    print(first_number*secound_number)
+elif operation=="/":
+    print(first_number/secound_number)
+    bus_capacity=input("enter the number of passengers")
+    people_inbus=input("enter the number of people who want to ride the bus")
